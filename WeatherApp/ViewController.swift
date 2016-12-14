8d55d6e4ce6ecdf952f752d5b60289137e63937c
Andrei Palonski <skerjie@live.com>
2016-12-14 23:35:17 +0300

    1st commit

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -7,11 +7,17 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+  var locationManager = CLLocationManager()
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    locationManager.delegate = self
     // Do any additional setup after loading the view, typically from a nib.
   }
 
@@ -19,7 +25,12 @@ class ViewController: UIViewController {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+  
+  func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+    print("location has changed")
+  }
 
+  
 
 }
 
